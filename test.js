0f16d5aa7715e1fd9af1b354a2358e1d55438178
Sam Rijs <srijs@airpost.net>
2016-06-10 17:05:39 +1000

    add extensive tests

--- test.js
+++ test.js
@@ -1,22 +1,69 @@
-var hash = require('./index');
+'use strict';
 
-var assert = require('assert');
-var crypto = require('crypto');
+const hash = require('./index');
 
-var type = 'Sha256';
-var hasher = new hash.Hash(hash.HashType[type]);
+const assert = require('assert');
+const crypto = require('crypto');
 
-var random1 = crypto.randomBytes(Math.floor(Math.random() * 1024 * 1024));
-var random2 = crypto.randomBytes(Math.floor(Math.random() * 1024 * 1024));
+const random = require('seed-random');
 
-var hash1 = crypto.createHash(type.toLowerCase())
-  .update(random1)
-  .update(random2)
-  .digest('hex');
-console.log(hash1);
+function repeat(n, action) {
+  if (n === 0) {
+    return Promise.resolve();
+  }
+  return action().then(() => repeat(n - 1, action));
+}
 
-hasher.update(random1).then(hasher2 => {
-  return hasher2.update(random2).then(hasher3 => {
-    return hasher3.digest();
+function randomBuffer(gen) {
+  const len = gen() * 1024 * 1024;
+  const buf = new Buffer(len);
+  for (let i = 0; i < len; i++) {
+    buf[i] = (gen() * 255) << 0;
+  }
+  return buf;
+}
+
+function testHashingWithTwoBuffers(gen, type) {
+  const random1 = randomBuffer(gen);
+  const random2 = randomBuffer(gen);
+
+  const hash1 = crypto.createHash(hash.HashType[type].toLowerCase())
+    .update(random1)
+    .update(random2)
+    .digest('hex');
+
+  const hasher = new hash.Hash(type);
+
+  return hasher.update(random1).then(hasher2 => {
+    return hasher2.update(random2).then(hasher3 => {
+      return hasher3.digest().then(digest => {
+        assert.equal(digest.toString('hex'), hash1);
+      })
+    });
   });
-}).then(res => console.log(res.toString('hex')), err => console.log(err));
+}
+
+const seed = crypto.randomBytes(8).toString('hex');
+const gen = random(seed);
+
+console.log('seed:', seed);
+
+describe('Hash', () => {
+
+  describe('Sha1', () => {
+
+    it('works with two buffers', () => {
+      return repeat(50, () => testHashingWithTwoBuffers(gen, hash.HashType.Sha1));
+    }).timeout(5000);
+
+  });
+
+  describe('Sha256', () => {
+
+    it('works with two buffers', () => {
+      return repeat(50, () => testHashingWithTwoBuffers(gen, hash.HashType.Sha256));
+    }).timeout(5000);
+
+  });
+
+});
