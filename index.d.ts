3a48d79a90b5a2fa23241de679d6c1dffd591be4
Sam Rijs <srijs@airpost.net>
2016-08-06 13:23:50 +1000

    fix updateSync type signature

--- index.d.ts
+++ index.d.ts
@@ -11,7 +11,7 @@ export class Hash {
   update(data: Buffer): Promise<Hash>;
   digest(): Promise<Buffer>;
 
-  updateSync(): Buffer;
+  updateSync(): Hash;
   digestSync(): Buffer;
 
   serialize(): Buffer;
